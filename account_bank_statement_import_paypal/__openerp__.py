ad7509f873421ae973692bc998a1d4990b48ceab
Adrien Peiffer (ACSONE) <adrien.peiffer@acsone.eu>
2016-09-14 18:19:11 +0200

    [FIX] Version

--- account_bank_statement_import_paypal/__openerp__.py
+++ account_bank_statement_import_paypal/__openerp__.py
@@ -22,7 +22,7 @@
 
 {
     'name': 'Import Paypal Bank Statements',
-    'version': '0.1',
+    'version': '8.0.1.0.0',
     'license': 'AGPL-3',
     'author': 'Akretion',
     'website': 'http://www.akretion.com',
